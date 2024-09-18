dc5159d2f6b138975378e9b133aecdf11edabf9e
Bablu8569 <kbablu2483@gmail.com>
2024-09-18 20:10:59 +0530

    Add files via upload

---
+++ MatplotlibPilot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[59,59,76]
+y=[2015,2017,2024]
+plt.pie(x)
+plt.title("Student Graph")
+plt.xlabel("Student porfence")
+plt.ylabel("Student")
+plt.plot()
+plt.show()
